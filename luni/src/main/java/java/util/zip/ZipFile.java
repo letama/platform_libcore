cba6cc25f6429a59ce32ca0a9a2ac0671aabd8f3
Philippe Larrieux <philippe.larrieux@gmail.com>
2013-07-24 16:47:36 +0200

    Fix 9695860 and 8219321 vulnerabilities
    Change-Id: I0699934aac5cffe9ea2aa3f9d8b159dc15c645b9

--- luni/src/main/java/java/util/zip/ZipFile.java
+++ luni/src/main/java/java/util/zip/ZipFile.java
@@ -259,7 +259,7 @@ public class ZipFile implements ZipConstants {
             // the one coming in the central header.
             RAFStream rafstrm = new RAFStream(raf, entry.mLocalHeaderRelOffset + 28);
             DataInputStream is = new DataInputStream(rafstrm);
-            int localExtraLenOrWhatever = Short.reverseBytes(is.readShort());
+            int localExtraLenOrWhatever = Short.reverseBytes(is.readShort())  & 0xffff;
             is.close();
 
             // Skip the name and this "extra" data or whatever it is:
@@ -363,7 +363,10 @@ public class ZipFile implements ZipConstants {
         byte[] hdrBuf = new byte[CENHDR]; // Reuse the same buffer for each entry.
         for (int i = 0; i < numEntries; ++i) {
             ZipEntry newEntry = new ZipEntry(hdrBuf, bin);
-            mEntries.put(newEntry.getName(), newEntry);
+	    String entryName = newEntry.getName();
+	    if (mEntries.put(entryName, newEntry) != null) {
+		throw new ZipException("Duplicate entry name: " + entryName);
+	    }
         }
     }
 
